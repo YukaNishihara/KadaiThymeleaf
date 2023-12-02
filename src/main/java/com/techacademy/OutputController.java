cca48b8c236dfee691defeecd3db96f1006f9cc8
YukaNishihara <bibi0129@gmail.com>
2023-12-02 21:20:01 +0900

    Lesson15課題

---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,21 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutputController {
+    
+    @PostMapping("/output")
+    public String postOutput(@RequestParam("val") String previous, Model model) {
+        
+        model.addAttribute("previous",previous);
+         
+        
+        return "output";
+    }
+    
+
+}
